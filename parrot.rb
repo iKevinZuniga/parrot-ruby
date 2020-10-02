79e9f656026e9d2b4c08ec3c44140c83340aebf0
iKevinZuniga <kevin@puresyncore.com>
2020-10-02 11:43:10 -0500

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,8 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-    return "Squawk!"
+def parrot(phrase = "Squawk!")
+    puts phrase
+    phrase
 end
+
 
 
 
